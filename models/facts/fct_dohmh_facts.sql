c4e64c9f6587c1b72e9b764b46086271639fde00
Lea Petrela <LEA.PETRELA@BARUCHMAIL.CUNY.EDU>
2025-12-08 23:17:20 +0000

    dohmh fct

--- models/facts/fct_dohmh_facts.sql
+++ models/facts/fct_dohmh_facts.sql
@@ -1,53 +1,14 @@
-{{
-    config(
-        materialized = 'table'
-    )
-}}
+{{ config(materialized='table') }}
 
-with restaurant_dimension as (
-    select *
-    from {{ ref('restaurant_dimension') }}
-),
-
-violation_dimension as (
-    select *
-    from {{ ref('violation_dimension') }}
-),
-
-cuisine_dimension as (
-    select *
-    from {{ ref('cuisine_dimension') }}
-),
-
-action_dimension as (
-    select *
-    from {{ ref('action_dimension') }}
-),
-
-location_dimension as (
-    select *
-    from {{ ref('location_dimension') }}
-),
-
-date_dimension as (
-    select *
-    from {{ ref('date_dimension') }}
-),
-
-all_inspections as (
-    select
+WITH source AS (
+    SELECT
         camis,
-        dba,
-        boro              as borough,
+        dba AS restaurant_name,
+        boro AS borough,
         building,
         street,
         zipcode,
-        community_board,
-        council_district,
-        census_tract,
-        latitude,
-        longitude,
-
+        phone,
         cuisine_description,
         inspection_date,
         action,
@@ -58,46 +19,94 @@ all_inspections as (
         grade,
         grade_date,
         record_date,
-        inspection_type
-    from {{ ref('raw_dohmh') }}
+        inspection_type,
+        latitude,
+        longitude
+    FROM {{ ref('raw_dohmh') }}
+    WHERE inspection_date IS NOT NULL
+),
+
+date_join AS (
+    SELECT
+        s.*,
+        d.date_dim_id AS inspection_date_dim_id
+    FROM source s
+    LEFT JOIN {{ ref('date_dimension') }} d
+        ON DATE(s.inspection_date) = d.full_date
+),
+
+location_join AS (
+    SELECT
+        dj.*,
+        ld.location_dim_id
+    FROM date_join dj
+    LEFT JOIN {{ ref('location_dimension') }} ld
+        ON dj.borough   = ld.borough
+       AND dj.zipcode   = ld.zipcode
+       AND dj.latitude  = ld.latitude
+       AND dj.longitude = ld.longitude
+),
+
+restaurant_join AS (
+    SELECT
+        lj.*,
+        rd.restaurant_dim_id
+    FROM location_join lj
+    LEFT JOIN {{ ref('restaurant_dimension') }} rd
+        ON lj.camis = rd.camis
+),
+
+cuisine_join AS (
+    SELECT
+        rj.*,
+        cd.cuisine_dim_id
+    FROM restaurant_join rj
+    LEFT JOIN {{ ref('cuisine_dimension') }} cd
+        ON rj.cuisine_description = cd.cuisine_description
+),
+
+action_join AS (
+    SELECT
+        cj.*,
+        ad.action_dim_id
+    FROM cuisine_join cj
+    LEFT JOIN {{ ref('action_dimension') }} ad
+        ON cj.action = ad.action
+),
+
+violation_join AS (
+    SELECT
+        aj.*,
+        vd.violation_dim_id
+    FROM action_join aj
+    LEFT JOIN {{ ref('violation_dimension') }} vd
+        ON aj.violation_code        = vd.violation_code
+       AND aj.violation_description = vd.violation_description
 )
 
-select
-    rd.restaurant_dim_id,
-    ld.location_dim_id,
-    vd.violation_dim_id,
-    cd.cuisine_dim_id,
-    ad.action_dim_id,
-    dd_inspection.date_dim_id as inspection_date_dim_id,
-
-    1            as inspection_count,
-    ai.score     as inspection_score,
-    ai.grade     as inspection_grade,
-    ai.critical_flag,
-    ai.inspection_type
-
-from all_inspections ai
-
-join restaurant_dimension rd
-  using (camis)
-
-join location_dimension ld
-  using (
-      borough,
-      zipcode,
-      community_board,
-      latitude,
-      longitude
-  )
-
-join violation_dimension vd
-  using (violation_code, violation_description)
-
-join cuisine_dimension cd
-  using (cuisine_description)
-
-join action_dimension ad
-  using (action)
-
-join date_dimension dd_inspection
-  on extract(date from ai.inspection_date) = dd_inspection.full_date
+SELECT
+    ROW_NUMBER() OVER (ORDER BY camis, inspection_date) AS dohmh_fact_id,
+    restaurant_dim_id,
+    location_dim_id,
+    cuisine_dim_id,
+    action_dim_id,
+    violation_dim_id,
+    inspection_date_dim_id,
+    camis,
+    restaurant_name,
+    score,
+    CASE WHEN critical_flag = 'Y' THEN 1 ELSE 0 END AS is_critical_violation,
+    borough,
+    building,
+    street,
+    zipcode,
+    cuisine_description,
+    action,
+    violation_code,
+    violation_description,
+    critical_flag,
+    grade,
+    inspection_type,
+    grade_date,
+    record_date
+FROM violation_join
