8bfc6a1bc6c536cc2ce5b9e1fedc2965adacba30
Lea Petrela <LEA.PETRELA@BARUCHMAIL.CUNY.EDU>
2025-12-08 18:04:28 +0000

    change to sql code to connect

--- models/raw_311_complaints.sql
+++ models/raw_311_complaints.sql
@@ -1,149 +1,135 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
+SELECT 
+    unique_key,
+    created_date,
+    agency_name,
+    complaint_type,
+    descriptor,
+    location_type,
+    incident_zip,
+    incident_address,
+    street_name,
+    address_type,
+    city,
+    borough,
+    latitude,
+    longitude,
+    location,
+    status,
+    community_board,
+    open_data_channel_type
+FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2020`
 
-WITH union_311 AS
-(
-    SELECT 
-        unique_key,
-        created_date,
-        agency_name,
-        complaint_type,
-        descriptor,
-        location_type,
-        incident_zip,
-        incident_address,
-        street_name,
-        address_type,
-        city,
-        borough,
-        latitude,
-        longitude,
-        location,
-        status,
-        community_board,
-        open_data_channel_type
-    FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2020`
+UNION ALL
 
-    UNION ALL
+SELECT 
+    unique_key,
+    created_date,
+    agency_name,
+    complaint_type,
+    descriptor,
+    location_type,
+    incident_zip,
+    incident_address,
+    street_name,
+    address_type,
+    city,
+    borough,
+    latitude,
+    longitude,
+    location,
+    status,
+    community_board,
+    open_data_channel_type
+FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2021`
 
-    SELECT 
-        unique_key,
-        created_date,
-        agency_name,
-        complaint_type,
-        descriptor,
-        location_type,
-        incident_zip,
-        incident_address,
-        street_name,
-        address_type,
-        city,
-        borough,
-        latitude,
-        longitude,
-        location,
-        status,
-        community_board,
-        open_data_channel_type
-    FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2021`
+UNION ALL
 
-    UNION ALL
+SELECT 
+    unique_key,
+    created_date,
+    agency_name,
+    complaint_type,
+    descriptor,
+    location_type,
+    incident_zip,
+    incident_address,
+    street_name,
+    address_type,
+    city,
+    borough,
+    latitude,
+    longitude,
+    location,
+    status,
+    community_board,
+    open_data_channel_type
+FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2022`
 
-    SELECT 
-        unique_key,
-        created_date,
-        agency_name,
-        complaint_type,
-        descriptor,
-        location_type,
-        incident_zip,
-        incident_address,
-        street_name,
-        address_type,
-        city,
-        borough,
-        latitude,
-        longitude,
-        location,
-        status,
-        community_board,
-        open_data_channel_type
-    FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2022`
+UNION ALL
 
-    UNION ALL
+SELECT 
+    unique_key,
+    created_date,
+    agency_name,
+    complaint_type,
+    descriptor,
+    location_type,
+    incident_zip,
+    incident_address,
+    street_name,
+    address_type,
+    city,
+    borough,
+    latitude,
+    longitude,
+    location,
+    status,
+    community_board,
+    open_data_channel_type
+FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2023`
 
-    SELECT 
-        unique_key,
-        created_date,
-        agency_name,
-        complaint_type,
-        descriptor,
-        location_type,
-        incident_zip,
-        incident_address,
-        street_name,
-        address_type,
-        city,
-        borough,
-        latitude,
-        longitude,
-        location,
-        status,
-        community_board,
-        open_data_channel_type
-    FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2023`
+UNION ALL
 
-    UNION ALL
+SELECT 
+    unique_key,
+    created_date,
+    agency_name,
+    complaint_type,
+    descriptor,
+    location_type,
+    incident_zip,
+    incident_address,
+    street_name,
+    address_type,
+    city,
+    borough,
+    latitude,
+    longitude,
+    location,
+    status,
+    community_board,
+    open_data_channel_type
+FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2024`
 
-    SELECT 
-        unique_key,
-        created_date,
-        agency_name,
-        complaint_type,
-        descriptor,
-        location_type,
-        incident_zip,
-        incident_address,
-        street_name,
-        address_type,
-        city,
-        borough,
-        latitude,
-        longitude,
-        location,
-        status,
-        community_board,
-        open_data_channel_type
-    FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2024`
+UNION ALL
 
-    UNION ALL
-
-    SELECT 
-        unique_key,
-        created_date,
-        agency_name,
-        complaint_type,
-        descriptor,
-        location_type,
-        incident_zip,
-        incident_address,
-        street_name,
-        address_type,
-        city,
-        borough,
-        latitude,
-        longitude,
-        location,
-        status,
-        community_board,
-        open_data_channel_type
-    FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2025`
-)
-
-SELECT
-    *,
-    current_timestamp() AS loaded_at
-FROM union_311
+SELECT 
+    unique_key,
+    created_date,
+    agency_name,
+    complaint_type,
+    descriptor,
+    location_type,
+    incident_zip,
+    incident_address,
+    street_name,
+    address_type,
+    city,
+    borough,
+    latitude,
+    longitude,
+    location,
+    status,
+    community_board,
+    open_data_channel_type
+FROM `theta-mile-479604-h9.nyc_food_safety_raw.complaints_2025`
