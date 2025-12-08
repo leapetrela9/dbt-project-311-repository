eb3d68a8b2f6ea3ecdc36dac3736d00819124c3e
Lea Petrela <LEA.PETRELA@BARUCHMAIL.CUNY.EDU>
2025-12-08 18:19:45 +0000

    sql change to recognize CAMIS

--- models/raw_dohmh.sql
+++ models/raw_dohmh.sql
@@ -1,135 +1,155 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+WITH union_dohmh AS
+(
+    SELECT
+        camis,
+        dba,
+        boro,
+        building,
+        street,
+        zipcode,
+        phone,
+        cuisine_description,
+        inspection_date,
+        action,
+        violation_code,
+        violation_description,
+        critical_flag,
+        score,
+        grade,
+        grade_date,
+        record_date,
+        inspection_type
+    FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2020`
+    WHERE camis != 'CAMIS'
+
+    UNION ALL
+
+    SELECT
+        camis,
+        dba,
+        boro,
+        building,
+        street,
+        zipcode,
+        phone,
+        cuisine_description,
+        inspection_date,
+        action,
+        violation_code,
+        violation_description,
+        critical_flag,
+        score,
+        grade,
+        grade_date,
+        record_date,
+        inspection_type
+    FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2021`
+    WHERE camis != 'CAMIS'
+
+    UNION ALL
+
+    SELECT
+        camis,
+        dba,
+        boro,
+        building,
+        street,
+        zipcode,
+        phone,
+        cuisine_description,
+        inspection_date,
+        action,
+        violation_code,
+        violation_description,
+        critical_flag,
+        score,
+        grade,
+        grade_date,
+        record_date,
+        inspection_type
+    FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2022`
+    WHERE camis != 'CAMIS'
+
+    UNION ALL
+
+    SELECT
+        camis,
+        dba,
+        boro,
+        building,
+        street,
+        zipcode,
+        phone,
+        cuisine_description,
+        inspection_date,
+        action,
+        violation_code,
+        violation_description,
+        critical_flag,
+        score,
+        grade,
+        grade_date,
+        record_date,
+        inspection_type
+    FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2023`
+    WHERE camis != 'CAMIS'
+
+    UNION ALL
+
+    SELECT
+        camis,
+        dba,
+        boro,
+        building,
+        street,
+        zipcode,
+        phone,
+        cuisine_description,
+        inspection_date,
+        action,
+        violation_code,
+        violation_description,
+        critical_flag,
+        score,
+        grade,
+        grade_date,
+        record_date,
+        inspection_type
+    FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2024`
+    WHERE camis != 'CAMIS'
+
+    UNION ALL
+
+    SELECT
+        camis,
+        dba,
+        boro,
+        building,
+        street,
+        zipcode,
+        phone,
+        cuisine_description,
+        inspection_date,
+        action,
+        violation_code,
+        violation_description,
+        critical_flag,
+        score,
+        grade,
+        grade_date,
+        record_date,
+        inspection_type
+    FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2025`
+    WHERE camis != 'CAMIS'
+)
+
 SELECT
-    camis,
-    dba,
-    boro,
-    building,
-    street,
-    zipcode,
-    phone,
-    cuisine_description,
-    inspection_date,
-    action,
-    violation_code,
-    violation_description,
-    critical_flag,
-    score,
-    grade,
-    grade_date,
-    record_date,
-    inspection_type
-FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2020`
-
-UNION ALL
-
-SELECT
-    camis,
-    dba,
-    boro,
-    building,
-    street,
-    zipcode,
-    phone,
-    cuisine_description,
-    inspection_date,
-    action,
-    violation_code,
-    violation_description,
-    critical_flag,
-    score,
-    grade,
-    grade_date,
-    record_date,
-    inspection_type
-FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2021`
-
-UNION ALL
-
-SELECT
-    camis,
-    dba,
-    boro,
-    building,
-    street,
-    zipcode,
-    phone,
-    cuisine_description,
-    inspection_date,
-    action,
-    violation_code,
-    violation_description,
-    critical_flag,
-    score,
-    grade,
-    grade_date,
-    record_date,
-    inspection_type
-FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2022`
-
-UNION ALL
-
-SELECT
-    camis,
-    dba,
-    boro,
-    building,
-    street,
-    zipcode,
-    phone,
-    cuisine_description,
-    inspection_date,
-    action,
-    violation_code,
-    violation_description,
-    critical_flag,
-    score,
-    grade,
-    grade_date,
-    record_date,
-    inspection_type
-FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2023`
-
-UNION ALL
-
-SELECT
-    camis,
-    dba,
-    boro,
-    building,
-    street,
-    zipcode,
-    phone,
-    cuisine_description,
-    inspection_date,
-    action,
-    violation_code,
-    violation_description,
-    critical_flag,
-    score,
-    grade,
-    grade_date,
-    record_date,
-    inspection_type
-FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2024`
-
-UNION ALL
-
-SELECT
-    camis,
-    dba,
-    boro,
-    building,
-    street,
-    zipcode,
-    phone,
-    cuisine_description,
-    inspection_date,
-    action,
-    violation_code,
-    violation_description,
-    critical_flag,
-    score,
-    grade,
-    grade_date,
-    record_date,
-    inspection_type
-FROM `theta-mile-479604-h9.nyc_food_safety_raw.dohmh_2025`
+    *,
+    current_timestamp() AS loaded_at
+FROM union_dohmh
